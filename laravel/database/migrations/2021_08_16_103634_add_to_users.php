ed37f34ca7ab4a7ae6069f54edc9e42ac02024dc
sms217 <minsung0316@naver.com>
2021-08-16 20:33:33 +0900

    added

--- laravel/database/migrations/2021_08_16_103634_add_to_users.php
+++ laravel/database/migrations/2021_08_16_103634_add_to_users.php
@@ -14,8 +14,7 @@ class AddToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('grade');
-            $table->string('address');
+            $table->string('address')->nullable();
         });
     }
 
