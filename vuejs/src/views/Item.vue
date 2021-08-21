923f9fee96e789c719fc0ecf4a4f47858bcb14c3
chli-jiseong <chlwltjd98@naver.com>
2021-08-21 17:16:28 +0900

    login button

--- vuejs/src/views/Item.vue
+++ vuejs/src/views/Item.vue
@@ -1,7 +1,52 @@
 <template>
-    <div>
-        ItemDetails
-    </div>
+    <v-container>
+        <v-container>
+            <v-row>
+                <v-col cols="3.5">
+                    1
+                </v-col>
+                <v-col cols="8">
+                    <div class="three">3</div>
+                    <div class="three">4</div>
+                    <div class="three">5</div>
+                </v-col>
+            </v-row>
+            
+        </v-container>
+        <v-divider></v-divider>
+        <v-container>
+            <h3>연관상품</h3>
+            <v-card>
+                <v-row class="row">
+                    <v-col v-for="a in 5" :key="a" class="col" cols="2">dsds</v-col>
+                </v-row>
+            </v-card>
+        </v-container><br><br>
+        <v-container>
+            <div class="kk2">
+                <v-tabs
+                fixed-tabs
+                background-color="indigo"
+                dark
+                >
+                    <v-tab
+
+                    >
+                        상품정보
+                    </v-tab>
+                    <v-tab
+
+                    >
+                        상품문의
+                    </v-tab>
+                    
+                </v-tabs>
+                <v-container class="qq">
+                </v-container>               
+            </div>
+        </v-container>
+    </v-container>
+    
 </template>
 
 <script>
@@ -9,3 +54,28 @@ export default {
     
 }
 </script>
+
+<style scoped>
+    .container {
+        max-width: 1000px;
+        padding: 0%;
+        margin-bottom: 10px;
+    }
+
+
+    .row{
+        background: brown;
+        margin-top: 5px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .col {
+        background: blueviolet;
+        margin: 3px;
+        height: 130px;
+    }
+    .qq{
+        background: burlywood;
+        height: 500px;
+    }
+</style>
