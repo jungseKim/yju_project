cf4ab4a2dad4b44a48cb1d36b2002180967f4546
sms217 <minsung0316@naver.com>
2021-08-28 15:24:09 +0900

    backup minseong

--- vuejs/src/views/MyPage.vue
+++ vuejs/src/views/MyPage.vue
@@ -16,24 +16,26 @@ export default {
     },
     methods: {
         deleteAccount() {
-            var code = Math.floor(Math.random()*899999+100000)
-            var inputCode = prompt(`탈퇴하려면 아래의 코드를 입력해주세요.\n${code}`)
-            if(code==inputCode){
+            var code = Math.floor(Math.random() * 899999 + 100000);
+            var inputCode = prompt(
+                `탈퇴하려면 아래의 코드를 입력해주세요.\n${code}`
+            );
+            if (code == inputCode) {
                 if (confirm("정말 회원 탈퇴하시겠습니까?")) {
                     axios.post("/deleteAccount").then((res) => {
-                        this.$store.dispatch("logout")
-                        const home = window.location.origin
-                        this.text = res.data
-                        alert(this.text)
-                        location.href = home
+                        this.$store.dispatch("logout");
+                        const home = window.location.origin;
+                        this.text = res.data;
+                        alert(this.text);
+                        location.href = home;
                     });
                 }
-            }else{
-                    alert('코드가 일치하지 않습니다.')
-                 }
+            } else {
+                alert("코드가 일치하지 않습니다.");
+            }
         },
     },
-}
+};
 </script>
 
 <style>
