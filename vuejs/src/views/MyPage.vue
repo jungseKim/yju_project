7fb199aad58e27c5d4b46b921bf11236c67efb81
sms217 <minsung0316@naver.com>
2021-08-28 15:17:57 +0900

    backup

--- vuejs/src/views/MyPage.vue
+++ vuejs/src/views/MyPage.vue
@@ -2,47 +2,45 @@
     <div>
         MyPage
         <v-btn @click="deleteAccount">회원탈퇴</v-btn>
-        <div>{{text}}</div>
+        <div>{{ text }}</div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    data(){
+    data() {
         return {
-            text : null
-        }
+            text: null,
+        };
     },
-    methods:{
-        deleteAccount(){
-            axios.post('/deleteAccount').then(res=>{
-                // this.text=res.data.id;
-                     this.$store.dispatch('logout')
-                    const home = window.location.origin;
-                    this.text=res.data;
-                    alert(this.text);
-                    // location.href=`${home}/api/logout`;
-                    location.href=home;
+    methods: {
+        deleteAccount() {
+            var code = Math.floor(Math.random()*899999+100000)
+            var inputCode = prompt(`탈퇴하려면 아래의 코드를 입력해주세요.\n${code}`)
+            if(code==inputCode){
+                if (confirm("정말 회원 탈퇴하시겠습니까?")) {
+                    axios.post("/deleteAccount").then((res) => {
+                        this.$store.dispatch("logout")
+                        const home = window.location.origin
+                        this.text = res.data
+                        alert(this.text)
+                        location.href = home
+                    });
                 }
-            )
-            // axios.post('/get').then(res=>{
-                //     console.log(res);
-            //     this.text=res.data;
-            //     }
-            // )
+            }else{
+                    alert('코드가 일치하지 않습니다.')
+                 }
         },
-    }
+    },
 }
 </script>
 
-
-
 <style>
 .profileImg {
-  width: 150px;
-  height: 150px;
-  border-radius: 70%;
-  overflow: hidden;
+    width: 150px;
+    height: 150px;
+    border-radius: 70%;
+    overflow: hidden;
 }
 </style>
